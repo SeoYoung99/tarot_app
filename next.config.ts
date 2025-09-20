8b28535a4825d03e66aeecbe900425bc293bc938
정서영 <jeongseoyeong@jeongseoyeong-ui-MacBookAir.local>
2025-09-20 16:00:56 +0900

    next.confing img size

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  api: {
+    bodyParser: {
+      sizeLimit: '5mb', // 이미지 크기에 따라 필요에 맞게 조정 가능
+    },
+  },
 };
 
 export default nextConfig;
